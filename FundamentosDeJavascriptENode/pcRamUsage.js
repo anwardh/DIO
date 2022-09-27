3377757fa39f7eddeb3da946c7877d6226a57840
Anwar <anwar@digitalhouse.com>
2022-09-26 22:09:00 -0300

    App para gerenciamento do uso da RAM

---
+++ FundamentosDeJavascriptENode/pcRamUsage.js
@@ -0,0 +1,26 @@
+const os = require ("os");
+
+setInterval (() => { // Esta função irá, a cada milisegundo, executar o códio em seu interior que lhe trará, em "tempo real", o status dos componentes do computador.
+
+    // console.log (os); -> Testando o Módulo OS
+
+    const {arch, platform, totalmem, freemem} = os; // Uso da "Desestruturação" para a retirada de alguns métodos contidos no OS
+    const tRam = totalmem()/1024/1024; // Para conversão em MB
+    const fRam = freemem()/1024/1024; // Para conversão em MB
+    const usage = (fRam / tRam) * 100; // Para verificar a porcentagem de RAM que está sendo consumida em tempo real
+
+
+
+    const stats = {
+        OS: platform(), // Identifica o tipo de Sis. Operacional
+        Arch: arch(), // Identifica os bytes da Placa-mãe
+        TotalRAM: `${parseInt(tRam)} MB`, // Identifica o Total de bytes da RAM (ocupados) passados pelo método "parseInt" em formato de String
+        Freemen: `${parseInt(fRam)} MB`, // Identifica o Total de bytes da RAM (livres) passados pelo método "parseInt" em formato de String
+        Usage: `${usage.toFixed(2)} %`, // Configura em 2 casas decimais o percentual de RAM consumidos em tempo real
+    }
+
+    console.clear(); // Para manter a tela do console "limpa"
+    console.table(stats); // Para imprimir na tela em forma de Tabela
+
+}, 1000);
+
